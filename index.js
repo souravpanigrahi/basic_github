dcae88a3ad752a4f4a1ab8528324ec5d4197bf6b
souravpanigrahi <souravpanigrahi2021@gmail.com>
2024-02-02 16:33:36 +0530

    update done

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const PORT = 3000;
+
+//Created a http server using createserver function using basic http module
+const server = http.createServer(function listener() {
+  //callback enabled with a listener fn
+    console.log("Request Recieved")
+});
+
+//Starts the server that we juts created above
+server.listen(PORT, function exec() {
+  console.log(`Server is up and running on:${PORT}`);
+});
