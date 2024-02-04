a6ad2e0ed6eb57f81d7f6f098767a1c3cae53178
souravpanigrahi <souravpanigrahi2021@gmail.com>
2024-02-05 02:52:32 +0530

    Souravs update

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const PORT = 3000;
 //Created a http server using createserver function using basic http module
 const server = http.createServer(function listener() {
   //callback enabled with a listener fn
-    console.log("Request Recieved")
+  console.log("Request Recieved");
 });
 
 //Starts the server that we juts created above
 server.listen(PORT, function exec() {
   console.log(`Server is up and running on:${PORT}`);
 });
+
+//Hi i have made some changes over here
+
+console;
+log("Hi this is the change introduced");
