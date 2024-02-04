c67b97d7f5a9d79a44b98839761ac5ea21e30f34
souravpanigrahi <souravpanigrahi2021@gmail.com>
2024-02-05 02:54:07 +0530

    New updates made in main branch

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const PORT = 3000;
 //Created a http server using createserver function using basic http module
 const server = http.createServer(function listener() {
   //callback enabled with a listener fn
-    console.log("Request Recieved")
+  console.log("Request Recieved");
 });
 
 //Starts the server that we juts created above
 server.listen(PORT, function exec() {
   console.log(`Server is up and running on:${PORT}`);
 });
+
+//Changes made via Main branch
+
+console.log("Hi these are changes made on the main branch");
